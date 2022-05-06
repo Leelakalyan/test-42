ff7f0510bcc082e675f5eec600c2aff2637a5ddd
Leelakalyan <99253536+Leelakalyan@users.noreply.github.com>
2022-05-06 20:50:02 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+c={1:[101,'kalyan','m','mccs'],2:[102,'ramesh','m','mpcs'],3:[103,'saraswathi','f','mccs']}
+x=int(input('enter the student no:'))
+print('sno:',c[x][0])
+print('name of the student:',c[x][1])
+print('gender:',c[x][2])
+print('group:',c[x][3])
+
+output:
+enter the student no:2
+sno: 102
+name of the student: ramesh
+gender: m
+group: mpcs
+      
